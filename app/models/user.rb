619edaeb32c6d8949dd5d47b2baef6daf904d582
stephanie pi <stephmpi@gmail.com>
2014-12-03 12:05:09 -0800

    password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :events
   has_many :tools
-  has_secure_password
 
   validates :name, presence: true
-  validates :password, length: { minimum: 4 }
-
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
+  has_secure_password validations: false
+  validates :password, length: { minimum: 4 }, confirmation: true, if: :password_sign_up?
 
 
 end
