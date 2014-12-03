9363145d92a077662a55d1ed3efe2ee929c4be46
stephanie pi <stephmpi@gmail.com>
2014-12-02 16:23:14 -0800

    create controllers and models

---
+++ db/migrate/20141203000842_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :uid
+      t.string :type
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
