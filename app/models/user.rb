57dd72f3af95921c9f86ad0cb0f115415d4fcd5a
stephanie pi <stephmpi@gmail.com>
2014-12-03 11:30:13 -0800

    add validations for name and email:

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :tools
   has_secure_password
+
+  validates :name, presence: true
+  validates :password, length: { minimum: 4 }
+
+  validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
 end
