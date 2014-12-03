458e6830c19c253ed63397b638d1641090f2fbbb
Allie Sterling <sterlina89@gmail.com>
2014-12-02 16:13:26 -0800

    adds tools controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203000017) do
+ActiveRecord::Schema.define(version: 20141203000940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20141203000017) do
   create_table "posts", force: true do |t|
     t.text     "content"
     t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tools", force: true do |t|
+    t.string   "type"
+    t.string   "status"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
