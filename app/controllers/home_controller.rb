e5a850ed0ffa50d58b7aac2a42d5dec293f33ba3
stephanie pi <stephmpi@gmail.com>
2014-12-03 16:55:55 -0800

    add login partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,25 @@
 class HomeController < ApplicationController
 
-  def index
-    @login_user = User.new
-
-  end
+  #
+  # def index
+  #   @login_user = User.new
+  #
+  # end
 
 
 
   def login
 
    @login_user = User.find_by(name: params[:user][:name])
-
       if  @login_user.authenticate(params[:user][:password])
-
         session[:user_id] = @login_user.id
         redirect_to root_path, :notice => "Welcome back, #{user.email}"
       else
         redirect_to root_path,  :notice => "Invalid email or password"
-
       end
+  end
 
+  def logout
   end
 
 
