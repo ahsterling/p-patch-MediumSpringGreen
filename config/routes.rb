ddad4b99568c6019fcc9e6908bd7b3cd4a0b604a
Allie Sterling <sterlina89@gmail.com>
2014-12-04 14:00:58 -0800

    adds index action to post controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get "/tools",       to: "tools#index", as: :tools
   put "/tools/:id",   to: "tools#update", as: :tool
+
+  get "/posts",       to: "posts#index", as: :posts
   root "home#index"
 
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, :type => :controller do
+describe PostsController do
+
+  describe 'GET #index' do
+    it 'is successful' do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
 
 end
