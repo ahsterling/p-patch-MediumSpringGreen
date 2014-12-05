f565a7a6b9e383ca5fbc3892c1769e41d3a59356
Allie Sterling <sterlina89@gmail.com>
2014-12-05 11:50:11 -0800

    changes tools index page

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,6 +1,6 @@
 
 module WeatherHelper
-  def weather_hash
+  def get_weather
     time = DateTime.parse($redis.get("time_of_call"))
     if Time.now - time >= 60
       weather = Wunderground.new(ENV["WUNDERGROUND_KEY"]).conditions_for("WA/Seattle")
@@ -9,56 +9,20 @@ module WeatherHelper
       $redis.set("icon", weather['current_observation']['icon_url'])
       $redis.set("time_of_call", weather['current_observation']["observation_time_rfc822"])
     end
-    # w_api.conditions_for("WA/Seattle")['current_oberservation']
-
-    # HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=seattle,wa").parsed_response
   end
 
   def temperature
-
-    weather_hash
+    get_weather
     $redis.get("temperature")
-    #weather_hash['current_observation']['temp_f']
-    # c = weather_hash['main']['temp'] - 273.15
-    # (((c * 9) / 5) + 32).to_i
   end
 
   def description
-    weather_hash
+    get_weather
     $redis.get("description")
-    #weather_hash['current_observation']['weather']
-
-    # weather_hash['weather'][0]['main']
-  end
-
-  def detailed_description
-    # weather_hash['weather'][0]['description']
-  end
-
-  def humidity
-    # weather_hash['main']['humidity']
-  end
-
-  def wind_speed
-    # weather_hash['wind']['speed']
   end
 
   def icon
-    weather_hash
+    get_weather
     $redis.get("icon")
-    #weather_hash['current_observation']['icon_url']
-    # if description == "Rain"
-    #   "cloudy-with-rain.png"
-    # elsif description == "Snow"
-    #   "heavy-snow.png"
-    # else
-    #   "sunny.png"
-    # end
-    # weather = weather_hash['main']['weather'][0]['main']
-    # if weather == "rain"
-    #   "cloudy-with-rain.png"
-    # else
-    #   "sunny.png"
-    # end
   end
 end
