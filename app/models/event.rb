94c34707ab3627c01e6a9cb351cd23b6bafcddd9
stephanie pi <stephmpi@gmail.com>
2014-12-05 14:23:34 -0800

    add events without user logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,22 @@ class Event < ActiveRecord::Base
 
   def self.sort_by_date
     Event.order(:date)
+
+  end
+
+  def future_event?
+    future_event = []
+    Event.sort_by_date.each do |e|
+      if e.date.future?
+        future_event << e
+      end
+      future_event
+    end
+
+  end
+
+  def ten_future_events
+    future_event?.arr[0..9]
   end
 
 
