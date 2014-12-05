502ee725666c1a78c88fc31cac15548eba1e4f2d
Allie Sterling <sterlina89@gmail.com>
2014-12-05 10:39:02 -0800

    implement weather underground gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem "omniauth"
 gem "omniauth-twitter"
 gem "faker"
 gem 'httparty'
+
+gem 'wunderground'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.5'
 # Use postgresql as the database for Active Record
--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,32 +1,50 @@
 
 module WeatherHelper
   def weather_hash
-    HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=seattle").parsed_response
+    Wunderground.new(ENV["WUNDERGROUND_KEY"]).conditions_for("WA/Seattle")
+    # w_api.conditions_for("WA/Seattle")['current_oberservation']
+
+    # HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=seattle,wa").parsed_response
   end
 
   def temperature
-    c = weather_hash['main']['temp'] - 273.15
-    (((c * 9) / 5) + 32).to_i
+    weather_hash['current_observation']['temp_f']
+    # c = weather_hash['main']['temp'] - 273.15
+    # (((c * 9) / 5) + 32).to_i
   end
 
   def description
-    weather_hash['weather'][0]['main']
+    weather_hash['current_observation']['weather']
+
+    # weather_hash['weather'][0]['main']
   end
 
   def detailed_description
-    weather_hash['weather'][0]['description']
+    # weather_hash['weather'][0]['description']
   end
 
   def humidity
-    weather_hash['main']['humidity']
+    # weather_hash['main']['humidity']
   end
 
   def wind_speed
-    weather_hash['wind']['speed']
+    # weather_hash['wind']['speed']
   end
 
   def icon
-    icon = weather_hash['weather'][0]['icon']
-    "http://openweathermap.org/img/w/#{icon}.png"
+    weather_hash['current_observation']['icon_url']
+    # if description == "Rain"
+    #   "cloudy-with-rain.png"
+    # elsif description == "Snow"
+    #   "heavy-snow.png"
+    # else
+    #   "sunny.png"
+    # end
+    # weather = weather_hash['main']['weather'][0]['main']
+    # if weather == "rain"
+    #   "cloudy-with-rain.png"
+    # else
+    #   "sunny.png"
+    # end
   end
 end
